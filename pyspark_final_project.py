5c5bb2022eb45135edd70a1cf652ff727c867efd
Venky Nagapudi <vnagapudi@berkeley.edu>
2019-08-02 22:32:44 -0700

    added other functions

--- pyspark_final_project.py
+++ pyspark_final_project.py
@@ -266,6 +266,296 @@ def makePrediction(dataRDD, W, treshProb=0.5):
     return (predlist)
 
 ################################################################################
+# Categorical variables
+################################################################################
+def getTop15FreqCols (rdd):
+    
+    #First get a count of rdd length
+    rddLen = rdd.count()
+
+    #Create pandas dataframe to store the top 15 values
+    top15df = pd.DataFrame(columns=['col', 'top15_values', 'top15_pct_contribution'])
+
+    for col in range(14,40):
+        catRdd = rdd.map(lambda x: x.split('\t')[col]) \
+                    .map(lambda x: (x,1)) \
+                    .reduceByKey(lambda x,y: x + y)
+    
+        freqRecord = catRdd.takeOrdered(15, key=lambda x: -x[1])
+        freqList = []
+        freqCnt = 0
+        for (k,v) in freqRecord:
+            freqCnt += v
+            freqList.append(str(k))
+        top15df = top15df.append({'col': col, 'top15_values': freqList, 'top15_pct_contribution': 100*freqCnt/rddLen}, ignore_index=True)
+    
+    return(top15df)
+
+
+
+def hashTrans (rdd):
+    def createHash (elem,hlen):
+        import mmh3
+        hashStr = []
+        for str in elem:
+            hashStr.append(mmh3.hash(str) % int(hlen))
+        return(hashStr)
+
+    def create1Hot(elem, hlen):
+        oneHotStr = []
+        #for hashStr in elem:
+        for i in range (hlen):
+            if (i == elem):
+                oneHotStr.append(1)
+            else:
+                oneHotStr.append(0)
+        return(oneHotStr)
+
+    def createCatArray(elem):
+        catArray = []
+        for array in elem:
+            for x in array:
+                catArray.append(x)
+        return(np.array(catArray))
+
+    #Define murmurHash level for 1-hot encoding
+    HASHLEN = 16
+
+    #testRdd.map(lambda x : x.split('\t')[14:40]).map(lambda x: [mmh3.hash(xn)%16 for xn in x]).take(5)
+    categoricalRdd = rdd.map(lambda x : x.split('\t')[14:40]) \
+                        .map(lambda x: createHash(x,HASHLEN)) \
+                        .map(lambda x: [create1Hot(xn, HASHLEN) for xn in x]) \
+                        .map(createCatArray)
+    return(categoricalRdd)
+
+def hybridFreqHashTrans (top15df, rdd):
+
+    #First create Broadcast variable
+    top15dfB = sc.broadcast(top15df)
+    
+    #set FreqThr at 90% for using frequency to bin the categories
+    TOP15_FREQ_THR = 90
+
+    #Define murmurHash level for 1-hot encoding
+    HASHLEN = 16
+    
+    #top15df = pd.DataFrame(columns=['col', 'top15_values', 'top15_pct_contribution'])
+    
+    def transformRow (row, top15df, hlen):
+        col = 0
+        transformedRow = []
+        
+        for x in row:
+            #Get frequency based map
+            if (top15df.loc[col]['top15_pct_contribution'] > 90):
+                xtrans = 15
+                for (ind,topElem) in enumerate(top15df.loc[col]['top15_values']):
+                    if (x == topElem):
+                        xtrans = ind
+            #else hash
+            else:
+                xtrans = mmh3.hash(x+str(col)) % int(hlen)
+        
+            transformedRow.append(xtrans)
+            
+            #Do this for all elements
+            col += 1
+        return (transformedRow)
+
+    def create1Hot(elem, hlen):
+        oneHotStr = []
+        #for hashStr in elem:
+        for i in range (hlen):
+            if (i == elem):
+                oneHotStr.append(1)
+            else:
+                oneHotStr.append(0)
+        return(oneHotStr)
+
+    def createCatArray(elem):
+        catArray = []
+        for array in elem:
+            for x in array:
+                catArray.append(x)
+        return(np.array(catArray))
+
+
+
+    categoricalRdd = rdd.map(lambda x : x.split('\t')[14:40]) \
+                        .map(lambda x: transformRow(x, top15dfB.value, HASHLEN)) \
+                        .map(lambda x: [create1Hot(xn, HASHLEN) for xn in x]) \
+                        .map(createCatArray)
+    return(categoricalRdd)
+
+def mergeNumPlusCatRdds(elem):
+    x, y = elem
+    xkey , xval = x
+    merge =  (xkey, np.hstack((xval,y)))
+    return(merge)
+
+################################################################################
+# FFM functions
+################################################################################
+
+def fmLoss(dataRDD, w, w1,w0) :
+    """
+    Computes the logloss given the data and model W
+    dataRDD - array of features, label
+    """
+    w_bc = sc.broadcast(w)
+    w1_bc = sc.broadcast(w1)
+    w0_bc = sc.broadcast(w0)
+    def probability_value(x,W,W1,W0): 
+        xa = np.array([x])
+        V =  xa.dot(W)
+        V_square = (xa*xa).dot(W*W)
+        phi = 0.5*(V*V - V_square).sum() + xa.dot(W1.T) + W0
+        return 1.0/(1.0 + np.exp(-phi))
+    
+    loss = dataRDD.map(lambda x: (x[0],x[1]) if x[0] == 1 else (-1, x[1])).map(lambda x:  (probability_value(x[1],w_bc.value, w1_bc.value, w0_bc.value), x[0])) \
+        .map(lambda x: (1 - 1e-12, x[1]) if x[0] == 1 else ((1e-12, x[1]) if x[0] == 0  else (x[0],x[1]))) \
+        .map(lambda x: -(x[1] * np.log(x[0]) + (1-x[1])*np.log(1-x[0]))).mean()
+    
+    
+    return float(loss)
+
+def fmGradUpdate_v1(dataRDD, w, w1, w0, alpha, regParam, regParam1, regParam0):
+    """
+    Computes the gradient and updates the model
+    """
+    
+    w_bc = sc.broadcast(w)
+    w1_bc = sc.broadcast(w1)
+    w0_bc = sc.broadcast(w0)
+    rp_bc = sc.broadcast(regParam)
+    rp1_bc = sc.broadcast(regParam1)
+    rp0_bc = sc.broadcast(regParam0)
+    
+    #Gradient for interaction term
+    
+    def row_grad(x, y, W, W1, W0, regParam, regParam1, regParam0):
+        xa = np.array([x])
+        VX =  xa.dot(W)
+        VX_square = (xa*xa).dot(W*W)
+        phi = 0.5*(VX*VX - VX_square).sum() + xa.dot(W1.T) + W0
+        expnyt = np.exp(y*phi) 
+        grad_loss = (-y/(1+expnyt))*(xa.T.dot(xa).dot(W) - np.diag(np.square(x)).dot(W))
+        return 2*regParam*W + grad_loss
+    
+    #Gradient for Linear term
+    def row_grad1(x, y, W, W1, W0, regParam, regParam1, regParam0):
+        xa = np.array([x])
+        VX =  xa.dot(W)
+        VX_square = (xa*xa).dot(W*W)
+        phi = 0.5*(VX*VX - VX_square).sum() + xa.dot(W1.T) + W0
+        expnyt = np.exp(y*phi)
+        grad_loss1 = (-y/(1+expnyt))*xa
+        return 2*regParam1*W1 + grad_loss1
+    
+    #Gradient for bias term
+    def row_grad0(x, y, W, W1, W0, regParam, regParam1, regParam0):
+        xa = np.array([x])
+        VX =  xa.dot(W)
+        VX_square = (xa*xa).dot(W*W)
+        phi = 0.5*(VX*VX - VX_square).sum() + xa.dot(W1.T) + W0
+        expnyt = np.exp(y*phi)
+        grad_loss0 = (-y/(1+expnyt))*1
+        return 2*regParam0*W0 +grad_loss0
+    
+    batchRDD = dataRDD.sample(False, 0.01, 2019)  
+    grad = batchRDD.map(lambda x: (x[0],x[1]) if x[0] == 1 else (-1, x[1])).map(lambda x: (1, row_grad(x[1], x[0], w_bc.value, w1_bc.value, w0_bc.value, rp_bc.value,rp1_bc.value,rp0_bc.value))).reduceByKey(lambda x,y: np.add(x,y))
+    model = w - alpha * grad.values().collect()[0] 
+    
+    grad1 = batchRDD.map(lambda x: (x[0],x[1]) if x[0] == 1 else (-1, x[1])).map(lambda x: (1, row_grad1(x[1], x[0], w_bc.value, w1_bc.value, w0_bc.value, rp_bc.value,rp1_bc.value,rp0_bc.value))).reduceByKey(lambda x,y: np.add(x,y))
+    model1 = w1 - alpha * grad1.values().collect()[0]
+    
+    grad0 = batchRDD.map(lambda x: (x[0],x[1]) if x[0] == 1 else (-1, x[1])).map(lambda x: (1, row_grad0(x[1], x[0], w_bc.value, w1_bc.value, w0_bc.value, rp_bc.value,rp1_bc.value,rp0_bc.value))).reduceByKey(lambda x,y: np.add(x,y))
+    model0 = w0 - alpha * grad0.values().collect()[0]
+    
+    return model, model1 ,model0
+
+def GradientDescent(trainRDD, testRDD, model, model1, model0, nSteps = 20, 
+                    learningRate = 0.01, regParam = 0.01,regParam1 = 0.01,regParam0 = 0.01, verbose = False):
+    """
+    Perform nSteps iterations of OLS gradient descent and 
+    track loss on a test and train set. Return lists of
+    test/train loss and the models themselves.
+    """
+    # initialize lists to track model performance
+    train_history, test_history, model_history, model1_history, model0_history = [], [], [], [], []
+    
+    # perform n updates & compute test and train loss after each
+    model = wInit
+    model1 = wInit1
+    model0 = wInit0
+    for idx in range(nSteps): 
+        
+        ############## YOUR CODE HERE #############
+        
+        model, model1, model0 = fmGradUpdate_v1(trainRDD, model, model1, model0, learningRate, regParam, regParam1, regParam0)
+        training_loss = fmLoss(trainRDD, model, model1, model0) 
+        test_loss = fmLoss(testRDD, model, model1, model0) 
+        ############## (END) YOUR CODE #############
+        # keep track of test/train loss for plotting
+        train_history.append(training_loss)
+        test_history.append(test_loss)
+        model_history.append(model)
+        model1_history.append(model1)
+        model0_history.append(model0)
+        
+        # console output if desired
+        #if verbose:
+        #    print("----------")
+        #    print(f"STEP: {idx+1}")
+        #    print(f"training loss: {training_loss}")
+        #    print(f"test loss: {test_loss}")
+        #    #print(f"Model: {[k for k in model]}")
+   
+    return train_history, test_history, model_history, model1_history, model0_history
+
+def wInitialization(dataRDD, factor):
+    nrFeat = len(dataRDD.first()[1])
+    np.random.seed(int(time.time())) 
+    w =  np.random.ranf((nrFeat, factor))
+    w = w / np.sqrt((w*w).sum())
+    
+    w1 =  np.random.ranf(nrFeat)
+    w1 = w1 / np.sqrt((w1*w1).sum())
+    
+    w0 =  np.random.ranf(1)
+    
+    return w, w1, w0
+
+def fmMakePrediction(dataRDD, w, w1, w0):
+    """
+    Perform one regularized gradient descent step/update.
+    Args:
+        dataRDD - records are tuples of (y, features_array)
+        W       - (array) model coefficients with bias at index 0
+    Returns:
+        pred - (rdd) predicted targets
+    """
+    w_bc = sc.broadcast(w)
+    w1_bc = sc.broadcast(w1)
+    w0_bc = sc.broadcast(w0)
+    def predict_fm(x, W, W1, W0):
+        xa = np.array([x])
+        VX =  xa.dot(W)
+        VX_square = (xa*xa).dot(W*W)
+        phi = 0.5*(VX*VX - VX_square).sum() + xa.dot(W1.T) + W0
+        return 1.0/(1.0 + np.exp(-phi))
+    
+    # compute prediction
+    pred = dataRDD.map(lambda x: (int(predict_fm(x[1],w_bc.value, w1_bc.value, w0_bc.value)>0.5), x[0] ))
+    ntp = pred.map(lambda x: int((x[0]*x[1]) == 1)).sum()
+    ntn = pred.map(lambda x: int((x[0]+x[1]) == 0)).sum()
+    nfp = pred.map(lambda x: int((x[0] == 1) * (x[1] == 0))).sum()
+    nfn = pred.map(lambda x: int((x[0] == 0) * (x[1] == 1))).sum()
+   
+    #return pred, ntp, ntn, nfp, nfn
+    return float(ntp), float(ntn), float(nfp), float(nfn)
+   
+################################################################################
 #High level options
 ################################################################################
 
@@ -355,6 +645,12 @@ train_sample = sc.textFile('data/sample_training.txt')\
                  .map(lambda x: x.split('\t'))\
                  .toDF().cache()
 
+#Read in the complete dataset
+train_sample_rdd = sc.textFile('data/sample_training.txt')
+
+#Get the top 1000 rows only (as before for numerical variables)
+testRdd = sc.parallelize(train_sample_rdd.take(10000),1)
+
 ################################################################################
 # Normalizing Numerical Variables
 ################################################################################
@@ -379,13 +675,21 @@ train_sample_red_RDD = train_sample_red.rdd.map(lambda x: (x[0], np.array(x[1:])
 #normalize features
 normedRDD = normalize(train_sample_red_RDD).cache()
 
-#print(normedRDD.take(3))
-
 #split into train, validation and test sets
-train, validation, test = normedRDD.randomSplit([0.6, 0.2, 0.2], seed=42)
+if (args.lr_opt == 0):
+    train, validation, test = normedRDD.randomSplit([0.6, 0.2, 0.2], seed=42)
+    numb_features = np.size(normedRDD.first()[1]) 
+elif (args.lr_opt == 1):
+    if (args.cat_opt == 0):
+        categoricalRDD = hashTrans(testRdd)
+    elif (args.cat_opt == 1):
+        top15df = getTop15FreqCols(train_sample_rdd)
+        categoricalRDD = hybridFreqHashTrans(top15df, testRdd)
+        numPlusCatRdd = normedRDD.zip(categoricalRdd) \
+                         .map(mergeNumPlusCatRdds)
+    train, validation, test = numPlusCatRDD.randomSplit([0.6, 0.2, 0.2], seed=42)
+    numb_features = np.size(numPlusCatRDD.first()[1]) 
 
-#compute the number of features
-numb_features = np.size(normedRDD.first()[1]) 
 #define baseline model, add one parameter representing the intercept
 BASELINE = np.random.randn(numb_features + 1)
 print (numb_features)
@@ -395,9 +699,15 @@ regType = str(args.hyp_reg_type)
 regParam = float(args.hyp_reg_param)
 learningRate = float(args.hyp_learning_rate)
 
-# run gradient descent
-train_loss, test_loss, model = GDUpdate(train, validation, BASELINE, nSteps, regType=regType, 
+
+if ((args.lr_opt == 0) or (args.lr_opt == 1)) :
+    # run gradient descent
+    train_loss, test_loss, model = GDUpdate(train, validation, BASELINE, nSteps, regType=regType, 
                                         regParam=regParam, learningRate=learningRate, verbose = False)
+else:
+    wInit, wInit1, wInit0 = wInitialization(train, 2)
+    logerr_train, logerr_test, models, model1s, model0s = GradientDescent(train, validation, wInit, wInit1, wInit0, nSteps = 100,
+                                                    learningRate = 0.002, regParam = 0.01, regParam1 = 0.01, regParam0 = 0.01, verbose = True)
 
 ##plt.plot(train_loss)
 ##plt.plot(test_loss)
@@ -405,7 +715,10 @@ train_loss, test_loss, model = GDUpdate(train, validation, BASELINE, nSteps, reg
 ##plt.show()
 
 #make predictions and compute metrics for treshProb = 0.5
-predlist = makePrediction(validation, model[-1], 0.5)
+if (args.lr_opt == 0) or (args.lr_opt == 1) :
+    predlist = makePrediction(validation, model[-1], 0.5)
+else:
+    ntp, ntn, nfp, nfn = fmMakePrediction(validation, models[-1], model1s[-1], model0s[-1])
 
 ntp = float(predlist[0])
 ntn = float(predlist[1])
